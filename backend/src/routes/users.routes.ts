c4ee21221ec8582c18e293e5306c68f002f1e0b2
Marcos de Alencar Carvalho <marcoscarvalho1997@gmail.com>
2020-08-12 10:03:55 -0300

    Teste deploy

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -21,4 +21,12 @@ usersRouter.post('/', async (request, response) => {
   }
 });
 
+usersRouter.get('/hello', (request, response) => {
+  try {
+    return response.json({ message: 'Hello' });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
 export default usersRouter;
